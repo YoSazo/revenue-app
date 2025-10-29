0eed649951900bfafadd1a10572948201b6b0c76
sazo <salahsalad100@gmail.com>
2025-10-29 10:17:42 -0500

    changes

--- api/insights.js
+++ api/insights.js
@@ -4,9 +4,9 @@ const accessToken = process.env.FB_ACCESS_TOKEN;
 const adAccountId = process.env.FB_AD_ACCOUNT_ID;
 
 const CAMPAIGN_NAME_MAP = {
-  '6911321772993': 'OKC',
+  '6913301535793': 'OKC',
   '6898612186993': 'BART',
-  '6911321773593': 'TULSA',
+  '6913301535993': 'TULSA',
 };
 const TARGET_CAMPAIGN_IDS = Object.keys(CAMPAIGN_NAME_MAP);
 
